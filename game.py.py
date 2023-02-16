da3e308325fdb9345c091834785ea5239c4dcf6c
derctyvbun <prudimak12@gmail.com>
2023-02-16 10:23:20 +0300

    game project

--- game.py.py
+++ game.py.py
@@ -4,11 +4,25 @@ name=input('What is your name?  ')
 print('Hello there ' + name +'!')
 # generate a random number
 from random import randint
-num=randint(10, 50)
+random_num=randint(10, 50)
 # input random no
 counter=0
 while counter < 5:
    num=eval(input('Enter number : '))
+   counter+=1 # same as counter=counter+1
+   if num<random_num:
+      print('Number is low')
+   elif num>random_num:
+      print('Number is high')
+   elif num==random_num:
+      break #breaks the loop
+print('Game over!')
+if num==random_num:
+   print('You win!')
+else:
+   print('You lost! The correct number is ')
+   print(random_num)
+
 # generate a random no.
 # check if the input is equal to generated no
 # using a while loop,check if input is equal to generated no.
